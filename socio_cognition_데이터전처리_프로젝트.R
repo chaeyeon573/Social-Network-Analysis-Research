191a7c818d90774e42ccdd65861c4c89b8491251
chaeyeon <cmdysdntqh08@gmail.com>
2025-09-02 21:59:34 +0900

    Fix: update data preprocessing script

--- socio_cognition_데이터전처리_프로젝트.R
+++ socio_cognition_데이터전처리_프로젝트.R
@@ -1,5 +1,6 @@
 # Load packages
-my_packages <- c("igraph", "readxl", "openxlsx", "tidyr", "dplyr", "plm","broom")
+my_packages <- c("igraph", "readxl", "openxlsx", "tidyr", "dplyr", "plm","broom", "knitr","fixest")
+
 
 ## Install each package if not installed
 for(package_name in my_packages) {
@@ -9,16 +10,14 @@ for(package_name in my_packages) {
   library(package_name, character.only = TRUE)
 }
 
-
 # Read KSHAP network data
 ## Extract variable names as characters from the second row of the file
 varnames <- read_excel(
-  # "K_NETWORK_unlocked (1).xlsx",
+  "K_NETWORK_unlocked (1).xlsx",
   col_names = FALSE,
   range = "A2:J2"
 ) %>% 
   as.character()
-
 
 ## Read the file starting from the 3rd row with column names extracted above
 kshap_all_wave_network <- read_excel(
@@ -37,12 +36,11 @@ kshap_w3_network <- kshap_all_wave_network |>
   filter(wave ==3)
 
 
-
 # Read KSHAP KOSSDA data
 ## Read the file
 kshap_w1 <- read_excel(
-  'kshap_w1_kossda.xlsx',
-  col_names = TRUE
+  'KSHAP_W1_KOSSDA.xlsx',
+  # col_names = TRUE
 )
 
 
@@ -50,7 +48,6 @@ kshap_w3 <- read_excel(
   'kshap_w3_kossda.xlsx',
   col_names = TRUE
 )
-
 
 
 # Section: Network Data Manipulation -wave1 &wave3
@@ -85,10 +82,6 @@ for (name in names(rename_list_w3)) {
     rename(!!name := !!sym(rename_list_w3[name]))
 }
 
-
-write.csv(kshap_w1_network_close_merged,"kshap_w1_network_close_merged.csv" )
-
-
 ## Pivot from wide to long with interpersonal closeness attached using tidyr
 kshap_w1_network_long <- pivot_longer(
   kshap_w1_network_close_merged,
@@ -107,8 +100,6 @@ kshap_w1_network_long <- pivot_longer(
          target = n_rnid_,
          weight = n_close_)
 
-
-## Pivot from wide to long with interpersonal closeness attached using tidyr
 kshap_w3_network_long <- pivot_longer(
   kshap_w3_network_close_merged,
   cols = c(starts_with("n_rnid_"), starts_with("n_close_")),
@@ -179,8 +170,7 @@ E(g_w3)[is.na(weight)]
 
 
 
-# Get WEIGHTED degrees and export them to excel with nid
-
+# Get network metrics
 network_metrics_w1 <- data.frame(
   rnid_ego        = as.numeric(V(g_w1)$name),
   degree_w_in     = strength(g_w1, mode = "in"),
@@ -189,19 +179,10 @@ network_metrics_w1 <- data.frame(
   betweenness     = betweenness(g_w1, directed = TRUE, weights = E(g_w1)$weight, normalized = TRUE),
   closeness       = closeness(g_w1, mode = "all", weights = E(g_w1)$weight),
   eigen_centrality= eigen_centrality(g_w1, directed = TRUE, weights = E(g_w1)$weight)$vector,
-  pagerank        = page_rank(g_w1, directed = TRUE, weights = E(g_w1)$weight)$vector,
   kcore_in        = coreness(g_w1, mode = "in"),
   kcore_out       = coreness(g_w1, mode = "out"),
   kcore_all       = coreness(g_w1, mode = "all")
 )
-hist(network_metrics_w1$degree_all)
-
-
-
-network_w1 <- left_join(kshap_w1_network_close_merged,network_metrics_w1, by = "rnid_ego" )
-wave1 <- full_join(kshap_w1, network_w1, by = "rdbid")
-
-sum(is.na(network_w1$rdbid))
 
 network_metrics_w3 <- data.frame(
   rnid_ego        = as.numeric(V(g_w3)$name),
@@ -211,15 +192,15 @@ network_metrics_w3 <- data.frame(
   # betweenness     = betweenness(g_w3, directed = TRUE, weights = E(g_w3)$weight),
   # closeness       = closeness(g_w3, mode = "all", weights = E(g_w3)$weight),
   # eigen_centrality= eigen_centrality(g_w3, directed = TRUE, weights = E(g_w3)$weight)$vector,
-  # pagerank        = page_rank(g_w3, directed = TRUE, weights = E(g_w3)$weight)$vector,
   # kcore_in        = coreness(g_w3, mode = "in"),
   # kcore_out       = coreness(g_w3, mode = "out"),
   # kcore_all       = coreness(g_w3, mode = "all"),
   # wave            = 3
 )
-dim(network_metrics_w3)
-dim(kshap_w3_network_close_merged)
-dim(network_w3)
+
+# Merge network metrics with KOSSDA data
+network_w1 <- left_join(kshap_w1_network_close_merged,network_metrics_w1, by = "rnid_ego" )
+wave1 <- full_join(kshap_w1, network_w1, by = "rdbid")
 
 network_w3 <- left_join(kshap_w3_network_close_merged,network_metrics_w3, by = "rnid_ego" )
 wave3 <- full_join(kshap_w3, network_w3, by = "rdbid")
@@ -229,10 +210,9 @@ wave3 <- full_join(kshap_w3, network_w3, by = "rdbid")
 #그러나, survey 데이터에는 rdbid값만이 존재하므로, 해당 노드의 rdbid값을 알수 있도록
 #network data(kshap_w1_network)에 degrees_df_w1을 병합합니다.
 
+# WAVE 1 변수 처리
 
-########################################################
-
-#wave 1: mmse 문항 선택
+# MMSE 문항 선택
 mmse_vars_w1 <- c(
   "a8501", "a8502", "a8503", "a8504", "a8505",
   "a8506", "a8507", "a8508", "a8509", "a8510",
@@ -241,19 +221,14 @@ mmse_vars_w1 <- c(
   "a883", "a891", "a892", "a90", "a911",
   "a912", "a913", "a92", "a93", "a94"
 )
-
-#wave 1 변수 처리
 wave1 <- wave1 |>
   mutate(
     wave = 1,
     #discussion network size
-    # nid = rnid_ego,
     network_size = n_size3,
-    
-    # 1. 배우자 여부
+    # 배우자
     spouse_num = ifelse(a062 %in% c(1, 2), 1, 0),
     spouse_label = factor(spouse_num, levels = c(0, 1), labels = c("No Spouse", "Has Spouse")),    
-    #통제변수
     sex = factor(sex, levels = c(1, 2), labels = c("Male", "Female")),
     edu = factor(case_when(
       a02 == 1 ~ "No formal education",
@@ -283,11 +258,8 @@ wave1 <- wave1 |>
         a48 == 2~ "Drinker"
       ), label = c("Non-Drinker","Drinker")
     ),
-    self_health = 6-b100,
-    #건강변수
-    diabetes = ifelse(dm_status == 2, 1, 0),
-    iadl_imp = ifelse(b104 >= 3, 1, 0),
-    
+    #전반적인 건강상태
+    self_health = 6-b100, 
     #우울증 수치
     # 1. 역코딩 (문항 a8405, a8410, a8415)
     across(c(a8405, a8410, a8415), ~ 5 - .),
@@ -297,16 +269,10 @@ wave1 <- wave1 |>
     cesd = rowSums(across(a8401:a8420), na.rm = TRUE),
     across(all_of(mmse_vars_w1), ~ ifelse(. == 9,0, .)),
     mmse = rowSums(across(all_of(mmse_vars_w1)), na.rm = FALSE),
-    #mmse
-    mmse_group = case_when(
-      mmse >= 20 ~ "치매 의심",
-      mmse < 20 ~ "정상",
-      TRUE~NA_character_
-    ))  |> 
-  mutate(
-    depressed = ifelse(cesd>=16, 1, 0),
-    depressed = factor(depressed, levels = c(0,1), labels = c("non-depressed","depressed"))
-)
+    ) 
+
+
+# wave3 변수처리
 
 #wave 3: mmse response 응답 문항 선택하기 
 mmse_vars_w3 <- c(
@@ -314,18 +280,15 @@ mmse_vars_w3 <- c(
   "l11", "l12", "l13", "l14", "l15", "l16", "l17", "l18", "l19", "l20",
   "l21", "l22", "l23", "l24", "l25", "l26", "l27", "l28", "l29", "l30"
 )
-# wave3 변수처리
+
 wave3 <- wave3 %>%
   mutate(
     wave = 3,
     # discussion network size
     network_size = n_size3,
-    
-    # 1. 배우자 여부
+    # 배우자
     spouse_num = ifelse(c3 %in% c(1, 2), 1, 0),
     spouse_label = factor(spouse_num, levels = c(0, 1), labels = c("No Spouse", "Has Spouse")),
-    
-    # 통제변수
     sex = factor(sex, levels = c(1, 2), labels = c("Male", "Female")),
     edu = factor(case_when(
       a2 == 1 ~ "No formal education",
@@ -357,10 +320,11 @@ wave3 <- wave3 %>%
       , levels = c(
         "Non-drinker", "Drinker"
       )),
+    #전반적인 건강상태
     self_health = ifelse(p1 == 9, NA, p1),
+    #CESD
     # 9를 NA로 처리
     across(k1_1:k1_20, ~ifelse(.==9, NA,.)),
-    
     # 역코딩
     k1_5  = ifelse(!is.na(k1_5), 5 - k1_5, NA),
     k1_10 = ifelse(!is.na(k1_10), 5 - k1_10, NA),
@@ -369,25 +333,13 @@ wave3 <- wave3 %>%
     across(k1_1:k1_20, ~ . - 1),
     #ces-d 점수 합산
     cesd = rowSums(across(k1_1:k1_20), na.rm = TRUE),
-    
     # MMSE 점수 계산, 각 항목을 0~3점으로 변환
     across(all_of(mmse_vars_w3), ~ ifelse(. == 9, 0, .)),
     mmse = rowSums(across(all_of(mmse_vars_w3)), na.rm = FALSE),
-    mmse_group = case_when(
-      !is.na(mmse) & mmse <= 20 ~ "치매 의심",
-      !is.na(mmse) & mmse > 20  ~ "정상",
-      TRUE ~ NA_character_
-    ),
-    
-    # 우울 여부
-    depressed = factor(ifelse(cesd >= 16, 1, 0),
-                       levels = c(0, 1),
-                       labels = c("non-depressed", "depressed"))
   )
 
 
-#########################################################
-
+# PANEL DATA 준비
 panel_long <- bind_rows(wave1, wave3) %>%
   select(
     rdbid, wave, rnid_ego,
@@ -408,30 +360,7 @@ panel_long <- bind_rows(wave1, wave3) %>%
 
 # 패널 데이터로 변환
 pdata <- pdata.frame(panel_long, index = c("rdbid","wave"))
-#########################################################
-# 원래 sex 변수의 unique 값 확인
 
-
-
-library(knitr)
-library(fixest)
-
-
-vars_base <- c("mmse", "cesd", "age", "sex", "edu", 
-               "income", "spouse_label", "degree_w_out", "self_health"
-)
-
-
-model_twoway <- plm(
-  log(mmse + 1) ~ cesd + self_health + spouse_label + age +
-    income + degree_w_out,
-  data = pdata,
-  model = "within",
-  effect = "twoways",
-  index = c("rdbid", "wave")
-)
-
-table(pdata$smoker)
 # FE (within)
 fe <- plm(
   log(mmse + 1) ~ cesd + self_health + spouse_label + age+
@@ -453,15 +382,9 @@ re <- plm(
 summary(re)
 
 phtest(fe,re)
-# 4) CRE 모델 (Mundlak approach)
-library(plm)
-library(dplyr)
 
-# 1. 변수 선택
-vars_cre <- c("cesd", "self_health", "spouse_label", "income_numeric",
-              "age", "income", "degree_w_out")
-
-# 2. 집단 평균(개인별 평균) 변수 추가
+# CRE
+# 집단 평균(개인별 평균) 변수 추가
 pdata_cre <- pdata %>%
   group_by(rdbid) %>%
   mutate(
@@ -473,7 +396,7 @@ pdata_cre <- pdata %>%
   ) %>%
   ungroup()
 
-# 3. CRE 모델 (RE + 평균 변수 추가)
+# CRE 모델 (RE + 평균 변수 추가)
 # 시간변수(변하는 변수)는 원자료 + 평균
 cre <- plm(
   log(mmse + 1) ~ cesd + mean_cesd +
@@ -481,12 +404,11 @@ cre <- plm(
      income_numeric + mean_income_numeric +
     degree_w_out + mean_degree_w_out +
     spouse_num + mean_spouse_num+
-    # sex + edu ,
+     sex + edu ,
   data = pdata_cre,
   model = "random",
   index = c("rdbid", "wave")
 )
-
 
 summary(cre)
 
